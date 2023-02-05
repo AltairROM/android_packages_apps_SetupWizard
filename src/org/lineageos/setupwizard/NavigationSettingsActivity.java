59ab2c98fce3778877b1435714fcf15c3b5e0337
Martin J. Pollard <martin0566@gmail.com>
2023-02-04 21:58:33 -0500

    SetupWizard: Add invert layout checkbox to navigation screen
    Allows the 3-button navigation layout to be inverted during setup.

--- src/org/lineageos/setupwizard/NavigationSettingsActivity.java
+++ src/org/lineageos/setupwizard/NavigationSettingsActivity.java
@@ -16,6 +16,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
@@ -29,11 +30,14 @@ import org.lineageos.setupwizard.util.SetupWizardUtils;
 
 public class NavigationSettingsActivity extends BaseSetupWizardActivity {
 
+    private static final String KEY_NAV_BAR_INVERSE = "sysui_nav_bar_inverse";
+
     private SetupWizardApp mSetupWizardApp;
 
     private String mSelection = NAV_BAR_MODE_GESTURAL_OVERLAY;
 
     private CheckBox mHideGesturalHint;
+    private CheckBox mInvertLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,16 +81,21 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
                 findViewById(R.id.navigation_illustration);
         final RadioGroup radioGroup = findViewById(R.id.navigation_radio_group);
         mHideGesturalHint = findViewById(R.id.hide_navigation_hint);
+        mInvertLayout = findViewById(R.id.invert_layout);
+
+        hideInvertCheckbox();
 
         radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
             if (checkedId == R.id.radio_gesture) {
                 mSelection = NAV_BAR_MODE_GESTURAL_OVERLAY;
                 navigationIllustration.setAnimation(R.raw.lottie_system_nav_fully_gestural);
                 revealHintCheckbox();
+                hideInvertCheckbox();
             } else if (checkedId == R.id.radio_sw_keys) {
                 mSelection = NAV_BAR_MODE_3BUTTON_OVERLAY;
                 navigationIllustration.setAnimation(R.raw.lottie_system_nav_3_button);
                 hideHintCheckBox();
+                revealInvertCheckbox();
             }
 
             navigationIllustration.playAnimation();
@@ -125,12 +134,48 @@ public class NavigationSettingsActivity extends BaseSetupWizardActivity {
                 });
     }
 
+    private void revealInvertCheckbox() {
+        mInvertLayout.animate().cancel();
+
+        if (mInvertLayout.getVisibility() == View.VISIBLE) {
+            return;
+        }
+
+        mInvertLayout.setVisibility(View.VISIBLE);
+        mInvertLayout.setAlpha(0.0f);
+        mInvertLayout.animate()
+            .translationY(0)
+            .alpha(1.0f)
+            .setListener(null);
+    }
+
+    private void hideInvertCheckbox() {
+        if (mInvertLayout.getVisibility() == View.INVISIBLE) {
+            return;
+        }
+
+        mInvertLayout.animate()
+            .translationY(-mInvertLayout.getHeight())
+            .alpha(0.0f)
+            .setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    super.onAnimationEnd(animation);
+                    mInvertLayout.setVisibility(View.INVISIBLE);
+                }
+            });
+    }
+
     @Override
     protected void onNextPressed() {
         mSetupWizardApp.getSettingsBundle().putString(NAVIGATION_OPTION_KEY, mSelection);
         boolean hideHint = mHideGesturalHint.isChecked();
         LineageSettings.System.putIntForUser(getContentResolver(),
                 LineageSettings.System.NAVIGATION_BAR_HINT, hideHint ? 0 : 1,
+                UserHandle.USER_CURRENT);
+        boolean invertLayout = mInvertLayout.isChecked();
+        Settings.Secure.putIntForUser(getContentResolver(),
+                KEY_NAV_BAR_INVERSE, invertLayout ? 1 : 0,
                 UserHandle.USER_CURRENT);
         super.onNextPressed();
     }
